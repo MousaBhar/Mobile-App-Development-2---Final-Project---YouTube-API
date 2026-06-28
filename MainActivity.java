6bf1fcf92111480784ae956dec9de37b05c0f945
Mousa I. Bhar <mousabhar10@gmail.com>
2026-06-28 07:59:04 +0300

    Delete MainActivity.java

--- MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.youtubefinalprojectmob2.adapter;
-
-import com.example.youtubefinalprojectmob2.model.Video;
-
-public interface OnVideoClickListener {
-    void onVideoClick(Video video);
-}
